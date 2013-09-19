9d2008168d266f629782d9ee4b90b0ee1fdcf36b
carliros <carliros.g@gmail.com>
2013-09-19 19:03:40 -0400

    Show only table and delete filter actions ...

--- site/src/app/views/library.js
+++ site/src/app/views/library.js
@@ -42,15 +42,14 @@ define(function(require) {
             $("#tableexample").append(grid.render().$el);
 
             this.collection.fetch({reset: true});
-            this.render();
+            /*this.render();*/
 
-            this.listenTo( this.collection, 'add', this.renderBook );
+            /*this.listenTo( this.collection, 'add', this.renderBook );*/
             this.listenTo( this.collection, 'reset', this.render );
         },
 
         events:{
               'click #add'   :'addBook'
-            , 'click #filter': 'filterBooks'
         },
 
         addBook: function( e ) {
@@ -78,47 +77,12 @@ define(function(require) {
             this.collection.create( formData );
         },
 
-        filterBooks: function ( e ){
-            e.preventDefault();
-
-            var formData = {};
-
-            $( '#addBook div' ).children( 'input' ).each( function( i, el ) {
-                if( $( el ).val() != '' ){
-                    if( el.id === 'keywords' ) {
-                        formData[ el.id ] = [];
-                        _.each( $( el ).val().split( ' ' ), function( keyword ) {
-                            formData[ el.id ].push({ 'keyword': keyword });
-                        });
-                    } else if( el.id === 'releaseDate' ) {
-                        formData[ el.id ] = $( '#releaseDate' ).datepicker( 'getDate' ).getTime();
-                    } else {
-                        formData[ el.id ] = $( el ).val();
-                    }
-                }
-                // Clear input field value
-                $( el ).val('');
-            });
-
-            var books = this.collection.fetch(
-                    { data: formData
-                    , type: 'POST'
-                    , success: function(d){
-                        console.log('fetch success');
-                    }});
-
-            console.log(formData);
-            console.log(books);
-
-            this.collection.reset(books);
-        },
-
         // render library by rendering each book in its collection
-        render: function() {
+        /*render: function() {
             this.collection.each(function( item ) {
                 this.renderBook( item );
             }, this );
-        },
+        },*/
 
         // render a book by creating a BookView and appending the
         // element it renders to the library's element
